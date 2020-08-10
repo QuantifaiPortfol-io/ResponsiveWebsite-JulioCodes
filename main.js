114919bd1f597afb6302b63cbd17b2308c6961d1
QuantifaiPortfol-io <quantifai.io@outlook.com>
2020-08-10 11:35:04 +0800

    Completed tutorial

--- main.js
+++ main.js
@@ -10,3 +10,36 @@ let body = selectElement('body');
 menuToggler.addEventListener('click', function () {
     body.classList.toggle('open');
 });
+
+// Scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left',  {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300,
+})
+
+sr.reveal('.animate-right',  {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+})
+
+sr.reveal('.animate-top',  {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+})
+
+sr.reveal('.animate-bottom',  {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+})
+
